cab9eae327271bf8efdc92da08f8e2642c98e62a
alex-blair <alex.n.blair@gmail.com>
2017-01-25 14:18:25 +1300

    add random board generator

--- minesweeper.js
+++ minesweeper.js
@@ -1,67 +1,38 @@
 document.addEventListener('DOMContentLoaded', startGame)
 
-// Define your `board` object here!
 var board = {
-  cells: [
-    {
-      row: 0,
-      col: 0,
-      isMine: false,
-      hidden: true
-    },
-    {
-      row: 0,
-      col: 1,
-      isMine: false,
-      hidden: true
-    },
-    {
-      row: 0,
-      col: 2,
-      isMine: true,
-      hidden: true
-    },
-    {
-      row: 1,
-      col: 0,
-      isMine: false,
-      hidden: true
-    },
-    {
-      row: 1,
-      col: 1,
-      isMine: false,
-      hidden: true
-    },
-    {
-      row: 1,
-      col: 2,
-      isMine: false,
-      hidden: true
-    },
-    {
-      row: 2,
-      col: 0,
-      isMine: false,
-      hidden: true
-    },
-    {
-      row: 2,
-      col: 1,
-      isMine: true,
-      hidden: true
-    },
-    {
-      row: 2,
-      col: 2,
-      isMine: true,
-      hidden: true
+  cells: []
+}
+
+function numberedCell (rowNum, colNum) {
+  var cell = {
+    row: rowNum,
+    col: colNum,
+    isMine: mineGenerator(),
+    hidden: true,
+    isMarked: false
+  }
+  return cell
+}
+
+function fillBoard (board, rows, cols) {
+  for (var i = 0; i < rows; i++) {
+    for (var j = 0; j < cols; j++) {
+      var newCell = numberedCell(i,j)
+      board.cells.push(newCell)
     }
-  ]
+  }
+
+  return board
+}
+
+function mineGenerator() {
+    return Math.random() < 0.2;
 }
 
 function startGame () {
-  for (var i = 0; i < 9; i++) {
+  fillBoard(board, 5, 5)
+  for (var i = 0; i < board.cells.length; i++) {
     board.cells[i].surroundingMines = countSurroundingMines(board.cells[i]);
   }
   document.getElementsByClassName('board')[0].addEventListener("click", checkForWin);
